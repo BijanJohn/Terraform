9ebd1c6515f952de81779e6a8488df2d2c571f0f
Bijan Rahnamai <bijan.rahnamai@q2ebanking.com>
2020-03-05 09:08:49 -0600

    separate dir project

---
+++ ch03/stage/data-stores/mysql/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "bijan-terraform-state"
+    key   = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+  dynamodb_table = "bijan-terraform-locks"
+  encrypt = true
+  }
+}
+
+
+resource "aws_db_instance" "example" {
+  instance_class = "db.t2.micro"
+  identifier_prefix = "bijan-terraform-example"
+  engine = "mysql"
+  allocated_storage = 10
+  name = "example_database"
+  username = "admin"
+
+  password = var.db_password
+  # This stores the password in AWS Secrets Manager
+  #password =
+  #  data.aws_secretsmanager_secret_version.db_password.secret_string
+}
+
+# This is one way to store the password
+#data "aws_secretsmanager_secret_version" "db_password" {
+#  secret_id = "mysql-master-password-stage"
+#}
